06c8822b61d64d307145e08241bef546aaec4af0
Dan Christian <dansilog16@gmail.com>
2021-11-20 20:43:14 +0545

    restart

--- no_sorter.py
+++ no_sorter.py
@@ -18,44 +18,20 @@ fourth = 0
 
 # If 
 # first statement
-if first_no < fourth_no and first_no < third_no and first_no < second_no:
+if first_no < second_no and first_no < third_no and first_no < fourth_no:
     first = first_no
-    if second_no < fourth_no and second_no < third_no and second_no < first_no:
-        first = second_no
-    if third_no < first_no and fourth_no < second_no and fourth_no < third_no:
-        first = third_no
-    else:
-        first = fourth_no
 
 # second statement
-elif first_no < second_no and first_no < third_no and first_no < fourth_no:
-    second = first_no
-    if second_no < first_no and second_no < third_no and second_no < fourth_no: 
-        second = second_no
-    if third_no < first_no and third_no < second_no and third_no < fourth_no:
-        second = third_no
-    else: 
-        second = fourth_no
+if second_no < first_no and second_no < third_no and second_no < fourth_no:
+    second_no = second_no
 
 # third statement
-elif first_no < second_no and first_no < third_no and first_no < fourth_no:
-    third = first_no
-    if second_no < first_no and second_no < third_no and second_no < fourth_no:
-        second = second_no
-    if third_no < first_no and third_no < second_no and third_no < third_no:
-        second = third_no
-    else:
-        second = fourth_no
+if third_no < first_no and third_no < second_no and third_no < fourth_no:
+    third = third_no
 
 # fourth statement
-elif first_no < second_no and first_no < third_no and first_no < fourth_no:
-    fourth = first_no
-    if second_no < first_no and second_no < third_no and second_no < fourth_no:
-        fourth_no = second_no
-    if third_no < first_no and third_no < second_no and third_no < fourth_no:
-        fourth_no = third_no
-    else:
-        fourth = fourth_no
+if fourth_no < first_no and fourth_no < second_no and fourth_no < third_no:
+    fourth_no = fourth_no
 
 
 # display
