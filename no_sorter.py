cad8931d75c003886ea05aa41b96f1cd8aba1a0a
Dan Christian <dansilog16@gmail.com>
2021-11-22 17:50:45 +0545

    restart

--- no_sorter.py
+++ no_sorter.py
@@ -10,35 +10,4 @@ third_no = int(input("Enter your third desired number: "))
 # ask for the fourth number
 fourth_no = int(input("Enter your fourth desired number: "))
 
-# first statement
-if first_no < second_no and first_no < third_no and first_no < fourth_no:
-    if second_no < third_no and second_no < fourth_no:
-        min, first_mid, second_mid, max = first_no, second_no, third_no, fourth_no
-    else: 
-        min, first_mid, second_mid, max = first_no, second_no, third_no, fourth_no
-
-# second statement
-elif second_no < first_no and second_no < third_no and second_no < fourth_no:
-    if first_no < third_no and first_no < fourth_no:
-        min, first_mid, second_mid, max = first_no, second_no, third_no, fourth_no
-    else:
-        min, first_mid, second_mid, max = first_no, second_no, third_no, fourth_no
-
-# third statement
-elif third_no < first_no and third_no < second_no and third_no < fourth_no:
-    if fourth_no < first_no and fourth_no < second_no:
-        min, first_mid, second_mid, max = first_no, second_no, third_no, fourth_no
-    else:
-        min, first_mid, second_mid, max = first_no, second_no, third_no, fourth_no
-
-# fourth statement
-else:
-    if first_no < second_no :
-        min, first_mid, second_mid, max = first_no, second_no, third_no, fourth_no
-    else:
-        min, first_mid, second_mid, max = first_no, second_no, third_no, fourth_no
-
-print("min : ", min)
-print("mid : ", first_mid)
-print("smid : ", second_mid)
-print("max : ", max)
+# Ill start all over again, got some guidance from a classmate
